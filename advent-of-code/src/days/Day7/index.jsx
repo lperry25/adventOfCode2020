d47e1179b4ca236a2705d8f90d5d71fd635276c1
Laura Perry <laura.perry@evry.com>
2020-12-07 12:33:12 +0100

    Day 7 part 2

--- advent-of-code/src/days/Day7/index.jsx
+++ advent-of-code/src/days/Day7/index.jsx
@@ -20,7 +20,7 @@ export function Day7() {
   let bagIndex = 0;
   do {
     bagRules = bagRules.filter(rule => {
-      const ruleSplit = rule.split('bags contain');
+      const ruleSplit = rule.split(' bags contain ');
       const includesTheBag = ruleSplit[1].includes(lookForBags[bagIndex]);
       if (includesTheBag){
         lookForBags.push(ruleSplit[0]);
@@ -31,14 +31,28 @@ export function Day7() {
   } while(bagIndex < lookForBags.length);
 
   const uniqueBags = lookForBags.filter(function(item, pos, self) {
-    return self.indexOf(item) == pos;
+    return self.indexOf(item) === pos;
   })
+
+  const bagsInBags = (lookFor) => {
+    const bagRule = allBagRules.find(rule => {
+      const ruleSplit = rule.split(' bags contain ');
+      return ruleSplit[0] == lookFor;
+    });
+    if (bagRule.includes('no other bags.')) return 0;
+    const bags = bagRule.split(' bags contain ')[1].split(', ');
+    return bags.map(bag => {
+      const numberOfBags = bag.charAt(0);
+      const bagType = bag.slice(2,bag.indexOf(' bag'));
+      return parseInt(numberOfBags,10) + parseInt(numberOfBags,10) * bagsInBags(bagType);
+    }).reduce((a,b) => a + b);
+  }
 
   return <div>
     {inputTextArea}
-    <p>There are {allBagRules.length} groups looking for help</p>
+    <p>There are {allBagRules.length} baggage rules</p>
     <p>There are <b>{uniqueBags.length - 1}</b> bags that can hold a shiny gold bag</p>
-    <p>There are <b>{uniqueBags.length - 1}</b> bags inside my shiny gold bag</p>
+    <p>There are <b>{bagsInBags('shiny gold')}</b> bags inside my shiny gold bag</p>
     <br/>
 
   </div>
