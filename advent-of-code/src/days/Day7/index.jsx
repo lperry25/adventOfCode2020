7c8deadd961966e6b8f907064a34d2e36ef33a44
Laura Perry <laura.perry@evry.com>
2020-12-07 12:46:23 +0100

    clean up code

--- advent-of-code/src/days/Day7/index.jsx
+++ advent-of-code/src/days/Day7/index.jsx
@@ -13,17 +13,16 @@ export function Day7() {
     </>;
 
   if (input === '') return inputTextArea;
-  const allBagRules =  input.split('\n');
+  const allBagRules =  input.split('\n').map(rule => rule.split(' bags contain '));
   let bagRules = allBagRules;
 
   let lookForBags = ['shiny gold'];
   let bagIndex = 0;
   do {
     bagRules = bagRules.filter(rule => {
-      const ruleSplit = rule.split(' bags contain ');
-      const includesTheBag = ruleSplit[1].includes(lookForBags[bagIndex]);
+      const includesTheBag = rule[1].includes(lookForBags[bagIndex]);
       if (includesTheBag){
-        lookForBags.push(ruleSplit[0]);
+        lookForBags.push(rule[0]);
       }
       return !includesTheBag;
     })
@@ -35,24 +34,21 @@ export function Day7() {
   })
 
   const bagsInBags = (lookFor) => {
-    const bagRule = allBagRules.find(rule => {
-      const ruleSplit = rule.split(' bags contain ');
-      return ruleSplit[0] == lookFor;
-    });
+    const bagRule = allBagRules.find(rule => rule[0] === lookFor);
     if (bagRule.includes('no other bags.')) return 0;
-    const bags = bagRule.split(' bags contain ')[1].split(', ');
+    const bags = bagRule[1].split(', ');
     return bags.map(bag => {
-      const numberOfBags = bag.charAt(0);
-      const bagType = bag.slice(2,bag.indexOf(' bag'));
-      return parseInt(numberOfBags,10) + parseInt(numberOfBags,10) * bagsInBags(bagType);
-    }).reduce((a,b) => a + b);
+      const numOfBags = parseInt(bag.charAt(0))
+      return numOfBags + numOfBags * bagsInBags(bag.slice(2,bag.indexOf(' bag')))
+    })
+      .reduce((a,b) => a + b);
   }
 
   return <div>
     {inputTextArea}
     <p>There are {allBagRules.length} baggage rules</p>
     <p>There are <b>{uniqueBags.length - 1}</b> bags that can hold a shiny gold bag</p>
-    <p>There are <b>{bagsInBags('shiny gold')}</b> bags inside my shiny gold bag</p>
+    <p>There are <b>{bagsInBags(lookForBags[0])}</b> bags inside my shiny gold bag</p>
     <br/>
 
   </div>
