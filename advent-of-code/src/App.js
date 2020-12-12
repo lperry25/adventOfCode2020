a9eb5a7a40805486b87625854a5a4f73c18f955e
Laura Perry <lauramperry.12@gmail.com>
2020-12-12 07:46:25 +0100

    working on router

--- advent-of-code/src/App.js
+++ advent-of-code/src/App.js
@@ -1,13 +1,26 @@
+import React from 'react';
+import { Router, Route, Switch } from "react-router";
 import './App.css';
+import {Day4} from "./days/Day4";
 import {Day11} from "./days/Day11";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Day11/>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Day11/>
+        </header>
+        <Switch>
+          <Route path='day4'>
+            <Day4/>
+          </Route>
+          <Route path='day11'>
+            <Day4/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
